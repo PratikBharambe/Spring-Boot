461cd4264f8c2a31dcae721d18c575418d643b27
SuraCdac <sura.cdac@gmail.com>
2024-12-10 05:49:07 +0530

    lombok used

--- SpringBootProductCRUDMVC/src/main/java/com/demo/SpringBootProductCRUDMVC/beans/Product.java
+++ SpringBootProductCRUDMVC/src/main/java/com/demo/SpringBootProductCRUDMVC/beans/Product.java
@@ -3,9 +3,15 @@ package com.demo.SpringBootProductCRUDMVC.beans;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "product1")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
 	@Id
@@ -14,63 +20,5 @@ public class Product {
 	private int qty;
 	private double price;
 	private int cid;
-
-	public Product() {
-		super();
-	}
-
-	public Product(int id, String name, int qty, double price, int cid) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.qty = qty;
-		this.price = price;
-		this.cid = cid;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getQty() {
-		return qty;
-	}
-
-	public void setQty(int qty) {
-		this.qty = qty;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public int getCid() {
-		return cid;
-	}
-
-	public void setCid(int cid) {
-		this.cid = cid;
-	}
-
-	@Override
-	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", qty=" + qty + ", price=" + price + ", cid=" + cid + "]";
-	}
 
 }
